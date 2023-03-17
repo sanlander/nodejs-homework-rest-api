a0da623609e90773a5368f9141ad66b14f075fa1
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2023-03-17 18:50:56 +0200

    Create Homework 02

---
+++ middlewares/validationContacts.js
@@ -0,0 +1,35 @@
+const Joi = require("joi");
+
+const regPhone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+
+const validateResult = (schema, req, res, next) => {
+  const result = schema.validate(req.body);
+  if (result.error) {
+    return res.status(400).json({ status: result.error.details });
+  }
+  return next();
+};
+
+module.exports = {
+  addPostValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string()
+        .required()
+        .pattern(/^[a-zA-Z0-9а-яА-Я ]{3,30}$/),
+      email: Joi.string().min(4).max(30).email().required(),
+      phone: Joi.string().min(7).max(30).required().pattern(regPhone),
+    });
+
+    validateResult(schema, req, res, next);
+  },
+
+  addPutValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().pattern(/^[a-zA-Z0-9а-яА-Я ]{3,30}$/),
+      email: Joi.string().min(4).max(30).email(),
+      phone: Joi.string().min(7).max(30).pattern(regPhone),
+    });
+
+    validateResult(schema, req, res, next);
+  },
+};
